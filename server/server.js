1732fe99353a89c974d749e4bb23365f98d8ca84
JAG <jesus.arribas.gomez@isgf.es>
2018-05-04 15:35:02 +0200

    medio google

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 const bodyParser = require('body-parser');
+const path = require("path");
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,6 +18,9 @@ app.use(require('./routes/index'))
 
 // parse application/json
 app.use(bodyParser.json())
+
+// habilitar carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')))
 
 
 
