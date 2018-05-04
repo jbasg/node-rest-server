fbe28a065910ea08e8fa16777405809db5708685
JAG <jesus.arribas.gomez@isgf.es>
2018-05-04 15:05:45 +0200

    Login JWT

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,7 @@
 
 process.env.PORT = process.env.PORT || 3000;
 
+
 //=======================
 // Entorno
 //=======================
@@ -11,10 +12,30 @@ let urlDB;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-if (process.env.NODE_ENV === 'dev'){
-  urlDB = 'mongodb://localhost:27017/cafe';
+//=======================
+// Vencimiento del token
+//=======================
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30 * 1000
+
+
+
+//=======================
+// SEED del token
+//=======================
+
+process.env.SEED = process.env.SEED || 'este-texto-es-el-seed'
+
+
+//=======================
+// Mongo DB
+//=======================
+
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = process.env.MONGO_URL;
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,13 +3,15 @@ const app = express();
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
+const { verificaToken, verificaAdmin_Role } = require('../middelwares/autenticacion');
 
-app.get('/usuario', (req, res) => {
+app.get('/usuario', verificaToken, (req, res) => {
+
     let desde = req.query.desde || 0;
     let limite = Number(req.query.limite || 5);
 
     desde = Number(desde);
-    Usuario.find({estado : true}, 'nombre email rol estado google img')
+    Usuario.find({ estado: true }, 'nombre email rol estado google img')
         .skip(desde)
         .limit(limite)
         .exec((err, usuarios) => {
@@ -32,7 +34,7 @@ app.get('/usuario', (req, res) => {
 
 })
 
-app.post('/usuario/', (req, res) => {
+app.post('/usuario/', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let body = req.body
 
@@ -56,7 +58,7 @@ app.post('/usuario/', (req, res) => {
 
 })
 
-app.put('/usuario/:id', (req, res) => {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
     //let body = req.body;
 
@@ -77,14 +79,14 @@ app.put('/usuario/:id', (req, res) => {
     })
 })
 
-app.delete('/usuario/:id', (req, res) => {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
-    let cambiaEstado = { 
-        estado : false
-    }
-    //Usuario.findByIdAndRemove(id, (err, usuario) => {
-    Usuario.findByIdAndUpdate(id,cambiaEstado,{ new : true },(err,usuario)=>{
+    let cambiaEstado = {
+            estado: false
+        }
+        //Usuario.findByIdAndRemove(id, (err, usuario) => {
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuario) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
